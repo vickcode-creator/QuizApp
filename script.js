f31da80cfa38f695692f58124294b37e43880e37
vickcode-creator <victormacharia002@gmail.com>
2024-11-25 20:49:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'Which country has won the most football world cups?',
+    answers: [
+      { text: 'Brazil', correct: true },
+      { text: 'Argentina', correct: false }
+    ]
+  },
+  {
+    question: 'Which country has the largest population in the world?',
+    answers: [
+      { text: 'India', correct: false },
+      { text: 'USA', correct: false },
+      { text: 'China', correct: true },
+      { text: 'Pakistan', correct: false }
+    ]
+  },
+  {
+    question: 'Which country has the largest GDP in the world ?',
+    answers: [
+      { text: 'USA', correct: true },
+      { text: 'China', correct: false },
+      { text: 'UK', correct: false },
+      { text: 'Singapore', correct: false }
+    ]
+  },
+  {
+    question: 'Which country has the largest GDP in Africa ?',
+    answers: [
+      { text: 'Nigeria', correct: true },
+      { text: 'Egypt', correct: false }
+    ]
+  }
+]
